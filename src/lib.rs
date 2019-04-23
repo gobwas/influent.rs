81a1debf8a8bec016e79ddda0f3837390a348775
Tuetuopay <tuetuopay@me.com>
2019-04-23 18:54:21 +0200

    lib: Add factory functions for reqwest clients

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub mod measurement;
 
 use client::Credentials;
 use client::http::HttpClient;
-use hurl::hyper::HyperHurl;
+use hurl::{hyper::HyperHurl, reqwest::{ReqwestHurl, ReqwestAsyncHurl}};
 use serializer::line::LineSerializer;
 
 /// Simple factory of `HttpClient` with `LineSerializer`
@@ -35,7 +35,9 @@ use serializer::line::LineSerializer;
 /// let client = create_client(credentials, vec!["http://localhost:8086"]);
 /// ```
 pub fn create_client<'a>(credentials: Credentials<'a>, hosts: Vec<&'a str>) -> HttpClient<'a> {
-    let mut client = HttpClient::new(credentials, Box::new(LineSerializer::new()), Box::new(HyperHurl::new()));
+    let mut client = HttpClient::new(credentials,
+                                     Box::new(LineSerializer::new()),
+                                     Box::new(HyperHurl::new()));
 
     for host in hosts {
         client.add_host(host);
@@ -44,3 +46,28 @@ pub fn create_client<'a>(credentials: Credentials<'a>, hosts: Vec<&'a str>) -> H
     client
 }
 
+pub fn create_reqwest_client<'a>(credentials: Credentials<'a>,
+                                 hosts: Vec<&'a str>) -> HttpClient<'a> {
+    let mut client = HttpClient::new(credentials,
+                                     Box::new(LineSerializer::new()),
+                                     Box::new(ReqwestHurl::new()));
+
+    for host in hosts {
+        client.add_host(host);
+    }
+
+    client
+}
+
+pub fn create_async_reqwest_client<'a>(credentials: Credentials<'a>,
+                                       hosts: Vec<&'a str>) -> HttpClient<'a> {
+    let mut client = HttpClient::new(credentials,
+                                     Box::new(LineSerializer::new()),
+                                     Box::new(ReqwestAsyncHurl::new()));
+
+    for host in hosts {
+        client.add_host(host);
+    }
+
+    client
+}
